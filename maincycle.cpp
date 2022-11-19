7f65c8144f412b2241fb8ee1f5f84c1db2ad5f80
minthukha-coder <112501430+minthukha-coder@users.noreply.github.com>
2022-11-19 17:37:48 +0630

    Create maincycle.cpp

---
+++ maincycle.cpp
@@ -0,0 +1,13 @@
+
+//
+// Created by User on 10/11/2022.
+//
+#include "cycle.h"
+#include"iostream"
+using namespace N;
+using namespace std;
+
+int main(){
+    cycle obj;
+    obj._integerloading();
+}
